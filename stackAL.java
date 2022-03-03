3cdbf80a83186fccc017ccea258494e3602e9faf
Yashwant Patel <39609184+Yashwant00P@users.noreply.github.com>
2022-03-03 23:44:43 +0530

    Update stackAL.java

--- stackAL.java
+++ stackAL.java
@@ -34,15 +34,5 @@ public class stackAL {
             return list.get(list.size()-1);
         }
     }
-    public static void main(String args[]) {
-        Stack stack = new Stack();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        while(!stack.isEmpty()) {
-            System.out.println(stack.peek());
-            stack.pop();
-        }
-    }
+    
 }
