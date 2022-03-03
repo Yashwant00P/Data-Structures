846a6a0bb2c2d84ba54e4db81686aeea5f4787f4
Yashwant Patel <39609184+Yashwant00P@users.noreply.github.com>
2022-03-03 23:43:27 +0530

    Add files via upload

---
+++ stackAL.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class stackAL {
+
+    // Creating stack as a class object.
+    static class Stack{
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // to check if stack is empty or not
+        public boolean isEmpty(){
+            return list.size() == 0;
+        }
+
+        // PUSH
+        public void push(int data){
+            list.add(data);
+        }
+
+        // POP
+        public int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top = list.remove(list.size()-1);
+            return top;
+        }
+
+        // PEEK
+        public int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            return list.get(list.size()-1);
+        }
+    }
+    public static void main(String args[]) {
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        while(!stack.isEmpty()) {
+            System.out.println(stack.peek());
+            stack.pop();
+        }
+    }
+}
